38a50cba85d7fb29f9e5599528f8415a1ecabac7
Paolo Perego <thesp0nge@gmail.com>
2013-07-03 08:43:21 +0200

    Added a twitter enabled feature support you can find in botolo v0.20

--- codesake-bot.rb
+++ codesake-bot.rb
@@ -2,13 +2,19 @@ module Botolo
   module Bot
     class Behaviour
       def initialize(options={})
-
       end
 
       def find_scanwithdawn
         Twitter.search("#scanwithdawn").results.each do |tweet|
           puts "#{tweet.from_user}: #{tweet.text}"
         end
+      end
+      def say_hello
+        $logger.log "hello"
+      end
+
+      def say_foo
+        $logger.log "foo"
       end
     end
   end
