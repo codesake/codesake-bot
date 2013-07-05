a2d8efb971b3e03303b02389b7e2f1a57bbfbd42
Paolo Perego <thesp0nge@gmail.com>
2013-07-05 07:57:07 +0200

    Added Datamapper Support and added an automatic update task ran every 6 h

--- codesake-bot.rb
+++ codesake-bot.rb
@@ -1,21 +1,76 @@
+require 'data_mapper'
+require 'dm-sqlite-adapter'
+require 'securerandom'
+
 module Botolo
   module Bot
+    # DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/scan_with_dawn.rb")
+
+    class Request
+      include DataMapper::Resource
+
+      property :id,           Serial
+      property :requested_by, String, :required => true
+      property :url,          String, :required => true
+      property :text,         String
+      property :created_at,   DateTime, :default=>DateTime.now
+      property :updated_at,   DateTime, :default=>DateTime.now
+    end
+    
     class Behaviour
       def initialize(options={})
-      end
-
-      def find_scanwithdawn
-        Twitter.search("#scanwithdawn").results.each do |tweet|
-          puts "#{tweet.from_user}: #{tweet.text}"
+        if ! options.empty? and ! options['bot']['db'].nil? and options['bot']['db']['enabled']
+          DataMapper.setup(:default, "sqlite3://#{File.join(Dir.pwd, options['bot']['db']['db_name'])}")
+          DataMapper.finalize
+          DataMapper.auto_migrate!
         end
       end
-      def say_hello
-        $logger.log "hello"
+
+      # https://github.com/codesake/codesake_dawn => https://t.co/vqZF4NpA7X
+      def find_scanwithdawn
+        Twitter.search("#scanwithdawn -rt").results.each do |tweet|
+          regexp = /https?:\/\/[\S]+/
+          if ! regexp.match(tweet.text).nil?
+            r = Botolo::Bot::Request.first(:url=>regexp.match(tweet.text))
+            if r.nil?
+              r = Botolo::Bot::Request.new
+              r.url = regexp.match(tweet.text)[0]
+              r.requested_by = tweet.from_user
+              r.text = tweet.text
+              saved = r.save
+              $logger.log "A new #scanwith dawn request created from #{r.requested_by}: #{r.url}" if saved
+              $logger.err "Can't save new #scanwith dawn request created from #{r.requested_by}: #{r.url}" if ! saved
+            else
+              $logger.log "Won't insert a duplicated request for url: #{r.url}"
+            end
+          else
+            $logger.log "Discarding tweet without URLs from #{tweet.from_user}: #{tweet.text}"
+          end
+        end
       end
 
-      def say_foo
-        $logger.log "foo"
+
+      def promote_dawn
+
+        fortunes = [
+          "Hey, check http://codesake.com. It will soon open to beta testers #dawnscanner #appsec services for #sinatra #codesake-bot", 
+          "Hey, check http://codesake.com. It will soon open to beta testers #dawnscanner #appsec services for #rails #codesake-bot", 
+          "Hey, check http://codesake.com. It will soon open to beta testers #dawnscanner #appsec services for #padrino #codesake-bot", 
+          "Did you know that #dawnscanner can make code reviews for web applications written in #rails, #sinatra and #padrino too? #codesake-bot",
+          "Did you know that #dawnscanner has more than 56 #cve #security checks? Run dawn -k and discover all of them #codesake-bot",
+          "Do you care about your web application security? Follow both @codesake and @armoredcode #codesake-bot", 
+          "If you care about your web application security you should really install #dawnscanner. Run gem install codesake-dawn now #codesake-bot",
+        ]
+          
+        message = fortunes[SecureRandom.random_number(fortunes.size)]
+        begin
+          Twitter.update(message)
+          $logger.log(message)
+        rescue => e
+          $logger.err("error tweeting #{message}: #{e.message}")
+        end
       end
+
     end
   end
 end
