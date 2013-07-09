81d6aa0e1870bc18a537a9f6f4705e6d7ae2a5d9
Paolo Perego <thesp0nge@gmail.com>
2013-07-09 08:27:15 +0200

    Added a fing_helobot behaviour and renamed codesake bot hashtag

--- codesake-bot.rb
+++ codesake-bot.rb
@@ -26,6 +26,19 @@ module Botolo
         end
       end
 
+      def find_helobot
+        Twitter.search("to: codesake #helobot", :result_type => "recent").results.map do |status|
+          $logger.log("saying hello to #{status.from_user} (original tweet: #{status.text}")
+          begin
+            Twitter.update("Hey @#{status.from_user}, what's going on? Tweet your url with #scanwithdawn to have it reviewed")
+          rescue => e
+            $logger.err("error tweeting #{message}: #{e.message}")
+          end
+
+        end
+
+      end
+
       # https://github.com/codesake/codesake_dawn => https://t.co/vqZF4NpA7X
       def find_scanwithdawn
         Twitter.search("#scanwithdawn -rt").results.each do |tweet|
@@ -49,17 +62,20 @@ module Botolo
         end
       end
 
+      def mark
+        $logger.log("codesake_bot is running with pid #{Process.pid}")
+      end
 
       def promote_dawn
 
         fortunes = [
-          "Hey, check http://codesake.com. It will soon open to beta testers #dawnscanner #appsec services for #sinatra #codesake-bot", 
-          "Hey, check http://codesake.com. It will soon open to beta testers #dawnscanner #appsec services for #rails #codesake-bot", 
-          "Hey, check http://codesake.com. It will soon open to beta testers #dawnscanner #appsec services for #padrino #codesake-bot", 
-          "Did you know that #dawnscanner can make code reviews for web applications written in #rails, #sinatra and #padrino too? #codesake-bot",
-          "Did you know that #dawnscanner has more than 56 #cve #security checks? Run dawn -k and discover all of them #codesake-bot",
-          "Do you care about your web application security? Follow both @codesake and @armoredcode #codesake-bot", 
-          "If you care about your web application security you should really install #dawnscanner. Run gem install codesake-dawn now #codesake-bot",
+          "Hey, check http://codesake.com. It will soon open to beta testers #dawnscanner #appsec services for #sinatra #codesake_bot", 
+          "Hey, check http://codesake.com. It will soon open to beta testers #dawnscanner #appsec services for #rails #codesake_bot", 
+          "Hey, check http://codesake.com. It will soon open to beta testers #dawnscanner #appsec services for #padrino #codesake_bot", 
+          "Did you know that #dawnscanner can make code reviews for web applications written in #rails, #sinatra and #padrino too? #codesake_bot",
+          "Did you know that #dawnscanner has more than 56 #cve #security checks? Run dawn -k and discover all of them #codesake_bot",
+          "Do you care about your web application security? Follow both @codesake and @armoredcode #codesake_bot", 
+          "If you care about your web application security you should really install #dawnscanner. Run gem install codesake-dawn now #codesake_bot",
         ]
           
         message = fortunes[SecureRandom.random_number(fortunes.size)]
