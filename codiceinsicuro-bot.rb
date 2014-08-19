30732d750324ddefdb3b35132fc2ef34cec14f57
Paolo Perego <thesp0nge@gmail.com>
2014-08-19 07:39:46 +0200

    removed empty?

--- codiceinsicuro-bot.rb
+++ codiceinsicuro-bot.rb
@@ -103,7 +103,7 @@ module Botolo
 
       def find_the_sp0nge_appsec(limit = 5)
         list = $twitter_client.search("from:thesp0nge #appsec")
-        unless list.nil? || list.empty? || list.to_a.nil?
+        unless list.nil? || list.to_a.nil?
 
           (0..limit-1).each do |l|
             t = list.to_a[SecureRandom.random_number(list.count)]
@@ -121,7 +121,7 @@ module Botolo
       end
       def find_appsec(limit = 5)
         list = $twitter_client.search("#appsec")
-        unless list.nil? || list.empty? || list.to_a.nil?
+        unless list.nil? || list.to_a.nil?
           (0..limit-1).each do |l|
             t = list.to_a[SecureRandom.random_number(list.count)]
             $logger.debug "retwitting #{t["from_user"]}: #{t["text"]}"
